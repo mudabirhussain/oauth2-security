2efec450185581809d322e299883afe0c99bd5e0
Mudabir Hussain <mudabirburfat97@gmail.com>
2024-09-13 16:22:34 +0400

    oauth2 google client added

--- src/main/java/com/security/oauth2_security/configs/SecurityConfig.java
+++ src/main/java/com/security/oauth2_security/configs/SecurityConfig.java
@@ -5,11 +5,30 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("mudabir")
+                        .password(
+                                bCryptPasswordEncoder().encode("secret"))
+                        .roles("USER")
+                        .build()
+        );
+    }
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
